c064fdff806bfeb95e991b64267a12cbb718933e
Akarshit <aakarshitgupta307@gmail.com>
2025-06-12 20:35:16 +0530

    minor bugs fixed and added a error handling middleware

--- app.js
+++ app.js
@@ -5,98 +5,90 @@ const Listing = require("./models/listing");
 const path = require("path");
 const { log } = require("console");
 const methodOverride = require("method-override");
-const ejsMate = require('ejs-mate');
-// const wrapAsync = require('./utils/wrapAsync.js');
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
 // const ExpressError = require('./utils/ExpressError.js');
 // const {listingSchema} = require("./schema.js");
 const bodyParser = require("body-parser");
 
-
 main()
-.then(() => {
-  console.log("connected to db");
-})
-.catch((err) => {
-  console.log(err);
-});
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/vistastays");
 }
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended : true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
   res.send("root page");
 });
-// index route 
+// index route
 app.get("/listings", async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("./listings/index.ejs",{allListings})
+  const allListings = await Listing.find({});
+  res.render("./listings/index.ejs", { allListings });
 });
 
 //create route
-app.get("/listings/new", (req,res)=>{
-    res.render("./listings/new.ejs");
-})
-app.post("/listings", async (req, res) => {
-  try {
-    const { listing } = req.body;
-
-    // Ensure image is in correct format for schema
-    const formattedListing = {
-      ...listing,
-      image: {
-        url: listing.image || "some link",
-        filename: "default", // default filename
-      },
-    };
-
-    const newListing = new Listing(formattedListing);
+app.get("/listings/new", (req, res) => {
+  res.render("./listings/new.ejs");
+});
+app.post("/listings", wrapAsync( async (req, res) => {
+    const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-  } catch (err) {
-    console.error("Error creating listing:", err);
-    res.send("Error creating listing: " + err.message);
-  }
-});
+}));
 
 
 //show route
-app.get("/listings/:id",(async( req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("./listings/show.ejs",{listing});
-}));
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/show.ejs", { listing });
+});
+
 //edit route
-app.get("/listings/:id/edit",( async(req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("./listings/edit.ejs",{listing});
-}));
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/edit.ejs", { listing });
+});
+
 //update route
-app.put("/listings/:id",(async( req,res)=>{
+app.put("/listings/:id", async (req, res) => {
   console.log(req.body);
   let { id } = req.params;
-   const data = req.body.listing;
+  const data = req.body.listing;
   if (typeof data.image === "string") {
-   data.image = { url: data.image };
- }
+    data.image = { url: data.image };
+  }
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
-}));
+});
+
 //delete route
-app.delete("/listings/:id",(async(req,res)=>{
-    let {id} = req.params;
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-}));
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+});
+
+app.use((err, req, res, next) => {
+res.send("Something went wrong, please try again later.");
+  // let { statusCode = 500, message = "error occured" } = err;
+  // res.status(statusCode).render("error.ejs", { message }); 
+});
+
 app.listen(8080, () => {
   console.log("server is running on port 8080");
 });
--- models/listing.js
+++ models/listing.js
@@ -1,15 +1,46 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const listingSchema = new Schema({
-    title:{type:String, required:true},
-    description:String,
-    image:{filename:{type:String},
-    url:{set:(v)=>v===""?"some link":v,type:String}},
-    price:Number,
-    location:String,
-    country:String
+const imageSchema = new Schema({
+  filename: {
+    type: String,
+    default: "default",
+  },
+  url: {
+    type: String,
+    default: "https://via.placeholder.com/600x400?text=No+Image",
+    set: (v) => v && v.trim() !== "" ? v : "https://via.placeholder.com/600x400?text=No+Image",
+  }
+});
 
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Description is required"],
+    trim: true,
+  },
+  image: {
+    type: imageSchema,
+    default: () => ({}),
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is required"],
+    min: [0, "Price must be positive"],
+  },
+  location: {
+    type: String,
+    required: [true, "Location is required"],
+  },
+  country: {
+    type: String,
+    required: [true, "Country is required"],
+  }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
